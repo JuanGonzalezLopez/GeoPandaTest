3d20201f423d45dc5526e38539921d3c8899eb8b
gustavocalderon <gustavo.calderon1@upr.edu>
2021-10-05 01:09:56 -0400

    Added cluster class

--- ClusterTest.py
+++ ClusterTest.py
@@ -1,88 +1,117 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial import distance_matrix
 from sklearn.cluster import KMeans
 import seaborn as sns; sns.set()
 import csv
 
-# Create DataFrame
-df = pd.read_csv('./ride_data/HullEx.csv')
-df = df[df['Latitude'] >= 17].reset_index(drop=True)  # Remove invalid coordinates
-df.dropna(axis = 0, how = 'any', subset=['Latitude', 'Longitude'], inplace = True)
-X=df.loc[:,['Latitude','Longitude']]
-print(X)
-
-# Elbow Curve: 12 Clusters
-# K_clusters = range(1,100)
-# kmeans = [KMeans(n_clusters=i) for i in K_clusters]
-# y_axis = df[['Latitude']]
-# x_axis = df[['Longitude']]
-# score = [kmeans[i].fit(y_axis).score(y_axis) for i in range(len(kmeans))]
-#
-# plt.plot(K_clusters, score)
-# plt.xlabel('Numero de Clusters')
-# plt.ylabel('Score')
-# plt.title('Elbow Curve')
-# plt.show()
+class Clusters:
+        # DF is the data from the rides
+        def __init__(self, df, clust = 12):
+                self.df = df
+                self.clust = clust
+        # Create DataFrame
+        def createCluster(self):
+                df = pd.read_csv('./ride_data/HullEx.csv')
+                df = df[df['Latitude'] >= 17].reset_index(drop=True)  # Remove invalid coordinates
+                self.df.dropna(axis = 0, how = 'any', subset=['Latitude', 'Longitude'], inplace = True)
+                X=self.df.loc[:,['Latitude','Longitude']]
+                print(X)
+                self.kmeans = KMeans(n_clusters=self.clust).fit(df)
+                self.centroids = self.kmeans.cluster_centers_
+                print(self.kmeans)
+                print(self.centroids)
 
 
-###################################################
+        def plotCluster(self):
+                self.createCluster()
+                plt.scatter(self.df['Longitude'], self.df['Latitude'], c = self.kmeans.labels_.astype(float),s=50, alpha=0.5)
+                plt.scatter(self.centroids[:, 1], self.centroids[:, 0], c = 'red', s=50)
+                plt.show()
 
-kmeans = KMeans(n_clusters=12).fit(df)
-print(kmeans)
-centroids = kmeans.cluster_centers_
-print(centroids)
-plt.scatter(df['Longitude'], df['Latitude'], c = kmeans.labels_.astype(float),s=50, alpha=0.5)
-plt.scatter(centroids[:, 1], centroids[:, 0], c = 'red', s=50)
-plt.show()
-
-###############################################
-
-
-
-
-
-
-
-
-# kmeans = KMeans(n_clusters = 30, init ='k-means++')
-# kmeans.fit(X[X.columns[1:3]]) # Compute k-means clustering.
-# X['cluster_label'] = kmeans.fit_predict(X[X.columns[1:3]])
-# centers = kmeans.cluster_centers_ # Coordinates of cluster centers.
-# labels = kmeans.predict(X[X.columns[1:3]])
-# X.head(10)
-
-# X.plot.scatter(x = 'latitude', y = 'longitude', c=labels, s=50, cmap='viridis')
-# plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+                # Elbow Curve: 12 Clusters
+                # K_clusters = range(1,100)
+                # kmeans = [KMeans(n_clusters=i) for i in K_clusters]
+                # y_axis = df[['Latitude']]
+                # x_axis = df[['Longitude']]
+                # score = [kmeans[i].fit(y_axis).score(y_axis) for i in range(len(kmeans))]
+                #
+                # plt.plot(K_clusters, score)
+                # plt.xlabel('Numero de Clusters')
+                # plt.ylabel('Score')
+                # plt.title('Elbow Curve')
+                # plt.show()
 
 
-###########################################################
+                ###################################################
 
-# f = pd.read_csv('skootel coordinate files')    #Read the dataframe file to get points with coordinate position
-# df.head(20)                                     #and give me the top 20 coordinates in the list
+                # Code to manually add the centroids
 
-
-# Randomly chose k examples as initial centroids from dataframe   #In our case we can do 30 as a naive count
-
-# Elbow Curve Chart algorithm               #With Elbow Curve algorithm we can see the actual number of k we
-                                            #need for the right amount of clusters
-
-
-# while not null:                               #While longitude and latitude not null
-  #  create k clusters by assigning each    #This will choose random points from data frame and make them into k
-   #     example to closest centroid        #ammount of centroids
-                                            #The closest points will change to the color closest to the centroid
-
-    # compute k new centroids by averaging  #Code will re-iterate the K-means algorithm until Elbow Curve chart
-      #  examples in each cluster          #changes are minimal.
-
-   # if centroids dont change:             #If centroids do not change after a while of re-iterating
-    #    break                             #finish code and print chart plot; K variable is found.
+                # data = [[18.2036534554253,-67.1432842412368],
+                #         [18.21090066197647,-67.140327357859],
+                #         [18.2143185762837,-67.1449586699717],
+                #         [18.2156944274901,-67.1476364135741],
+                #         [18.20834467867308,-67.14036502661509],
+                #         [18.21396222131919,-67.14547342452374],
+                #         [18.21087121410492,-67.13953130870321],
+                #         [18.21116007930294,-67.14133848752897],
+                #         [18.21698746774393,-67.1424664925079],
+                #         [18.21255278220406,-67.14108760783827]]
+                # points = np.array(data)
+                # distance_matrix(points, points)
+                # centroid_idx = [0,1,2,3,4,5,6,7,8,9]
+                # centroids = points[centroid_idx,:]
+                #####################################################
 
 
-# while not null:
-  #  create k (Known value) clusters by assigning each   #Now that K is known, iterate until k ammount
-   #     example to closes centroid
+                ###############################################
 
-    # plot cluster chart
-    # #Print cluster chart containing point and clusters
+
+
+
+
+
+
+
+                # kmeans = KMeans(n_clusters = 30, init ='k-means++')
+                # kmeans.fit(X[X.columns[1:3]]) # Compute k-means clustering.
+                # X['cluster_label'] = kmeans.fit_predict(X[X.columns[1:3]])
+                # centers = kmeans.cluster_centers_ # Coordinates of cluster centers.
+                # labels = kmeans.predict(X[X.columns[1:3]])
+                # X.head(10)
+
+                # X.plot.scatter(x = 'latitude', y = 'longitude', c=labels, s=50, cmap='viridis')
+                # plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+
+
+                ###########################################################
+
+                # f = pd.read_csv('skootel coordinate files')    #Read the dataframe file to get points with coordinate position
+                # df.head(20)                                     #and give me the top 20 coordinates in the list
+
+
+                # Randomly chose k examples as initial centroids from dataframe   #In our case we can do 30 as a naive count
+
+                # Elbow Curve Chart algorithm               #With Elbow Curve algorithm we can see the actual number of k we
+                                                            #need for the right amount of clusters
+
+
+                # while not null:                               #While longitude and latitude not null
+                  #  create k clusters by assigning each    #This will choose random points from data frame and make them into k
+                   #     example to closest centroid        #ammount of centroids
+                                                            #The closest points will change to the color closest to the centroid
+
+                    # compute k new centroids by averaging  #Code will re-iterate the K-means algorithm until Elbow Curve chart
+                      #  examples in each cluster          #changes are minimal.
+
+                   # if centroids dont change:             #If centroids do not change after a while of re-iterating
+                    #    break                             #finish code and print chart plot; K variable is found.
+
+
+                # while not null:
+                  #  create k (Known value) clusters by assigning each   #Now that K is known, iterate until k ammount
+                   #     example to closes centroid
+
+                    # plot cluster chart
+                    # #Print cluster chart containing point and clusters
--- Pseudo 2 EB.py
+++ Pseudo 2 EB.py
@@ -10,26 +10,26 @@ df = pd.read_csv('./ride_data/HullEx.csv')
 df = df[df['Latitude'] >= 17].reset_index(drop=True)  # Remove invalid coordinates
 df.dropna(axis = 0, how = 'any', subset=['Latitude', 'Longitude'], inplace = True)
 Y=df.loc[:,['Latitude','Longitude']]
-print(Y)
-
-# data = [[1, 0, 0],
-#         [1, 0.2, 0],
-#         [0, 0, 1],
-#         [0, 0, 0.9],
-#         [1, 0, 0.1]]
+# print(Y)
+# data = [[18.462746237, -67.4938764],
+#         [18.34783, -67.473846738],
+#         [18.378247, -67.4387583],
+#         [18.7483568, -67.3856836],
+#         [18.8978973, -67.583563856]]
 
 X = np.array(Y)
 
 distance_matrix(X,X)
 
-centroid_idx = [0,2] # let data point 0 and 2 be our centroids
+centroid_idx = [0,2,4] # let data point 0 and 2 be our centroids
+# print(centroid_idx)
 centroids = X[centroid_idx,:]
 print(centroids)
 
-kmeans = KMeans(n_clusters=2, init=centroids) # just run one k-Means iteration so that the centroids are not updated
+kmeans = KMeans(n_clusters=3, init=centroids) # just run one k-Means iteration so that the centroids are not updated
 
 kmeans.fit(X)
 kmeans.labels_
 plt.scatter(centroids[:, 1], centroids[:, 0], c = 'red', s=50)
-# plt.plot()
+plt.plot()
 plt.show()
